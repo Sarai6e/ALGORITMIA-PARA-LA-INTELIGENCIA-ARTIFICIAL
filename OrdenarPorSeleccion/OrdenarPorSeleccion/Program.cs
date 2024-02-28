5087475b048689a2ff703d17674ecb0236653a98
Sarai6e <1472173@senati.pe>
2024-02-28 08:03:05 -0500

    ..

---
+++ OrdenarPorSeleccion/OrdenarPorSeleccion/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class Program
+{
+    public static void OrdenarPorSeleccion(int[] array)
+    {
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            int indiceMinimo = i;
+            for (int j = i + 1; j < array.Length; j++)
+            {
+                if (array[j] < array[indiceMinimo])
+                {
+                    indiceMinimo = j;
+                }
+            }
+
+            if (indiceMinimo != i)
+            {
+                int temp = array[i];
+                array[i] = array[indiceMinimo];
+                array[indiceMinimo] = temp;
+            }
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        int[] numeros = { 5, 2, 4, 6, 1, 3 };
+
+        Console.WriteLine("Arreglo original:");
+        foreach (int numero in numeros)
+        {
+            Console.Write(numero + " ");
+        }
+
+        Console.WriteLine();
+
+        OrdenarPorSeleccion(numeros);
+
+        Console.WriteLine("Arreglo ordenado:");
+        foreach (int numero in numeros)
+        {
+            Console.Write(numero + " ");
+        }
+
+        Console.WriteLine();
+    }
+}
